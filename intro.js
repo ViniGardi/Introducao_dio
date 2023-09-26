077d69e10812e54adf1a0e7d5f4e94e24c5e590b
Vinicius Gardi <131396989+ViniGardi@users.noreply.github.com>
2023-09-26 12:21:35 -0300

    Update intro.js

---
+++ intro.js
@@ -0,0 +1,20 @@
+let nome = "Guardião Gardi"
+let experiencia = 7000
+
+if (experiencia <= 1000){
+    console.log("O Herói de nome " + nome + " está no nivel de Ferro")
+} else if(experiencia <= 2000){
+    console.log("O Herói de nome " + nome + " está no nivel de Bronze")
+} else if(experiencia <= 5000){
+    console.log("O Herói de nome " + nome + " está no nivel de Prata")
+}else if(experiencia <= 7000){
+    console.log("O Herói de nome " + nome + " está no nivel de Ouro")
+}else if(experiencia <= 8000){
+    console.log("O Herói de nome " + nome + " está no nivel de Platina")
+}else if(experiencia <= 9000){
+    console.log("O Herói de nome " + nome + " está no nivel de Ascendente")
+}else if(experiencia <= 10000){
+    console.log("O Herói de nome " + nome + " está no nivel de Imortal")
+} else {
+    console.log("O Herói de nome " + nome + " está no nivel de Radiante")
+}
